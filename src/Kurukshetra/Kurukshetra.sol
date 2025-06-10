6ca1bd140a580b04ab39b6e1e986db778c62a83a
yug49 <148035793+yug49@users.noreply.github.com>
2025-06-10 21:33:30 +0530

    RannToken.sol ✅

--- src/Kurukshetra/Kurukshetra.sol
+++ src/Kurukshetra/Kurukshetra.sol
@@ -66,6 +66,7 @@ import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable
 contract Kurukshetra is Ownable{
     error Kurukshetra__NotValidBridgeAddress();
     error Kurukshetra__GameNotStartedYet();
+    error Kurukshetra__GameFinishConditionNotMet();
 
     enum RankCategory{
         UNRANKED,
@@ -83,19 +84,23 @@ contract Kurukshetra is Ownable{
         RECOVER // defence + charisma
     }
 
-    RankCategory private immutable s_rankCategory;
+    RankCategory private immutable i_rankCategory;
+    uint256 private immutable i_costToInfluence;
+    uint256 private immutable i_costToDefluence;
     uint256 private s_playerOneNFTId;
     uint256 private s_playerTwoNFTId;
     address private s_playersBridgeAddress;
     uint8 private s_currentRound;
     address[] private s_playerOneBetAddresses;
-    uint256[] private s_playerOneBetAmounts;
+    mapping(address => uint256) private s_playerOneBetAmounts;
     address[] private s_playerTwoBetAddresses;
-    uint256[] private s_playerTwoBetAmounts;
+    mapping(address => uint256) private s_playerTwoBetAmounts;
     uint256 private s_playerOneInfluenceCost;
     uint256 private s_playerTwoInfluenceCost;
     uint256 private s_playerOneDefluenceCost;
     uint256 private s_playerTwoDefluenceCost;
+    mapping(address => bool) private s_playersAlreadyUsedDefluenceAddresses;
+    bool private s_gameInitialized;
 
     modifier onlyPlayersBridge() {
         if(msg.sender != s_playersBridgeAddress) {
@@ -104,54 +109,69 @@ contract Kurukshetra is Ownable{
         _;
     }
 
-    constructor(RankCategory _rankCategory) Ownable(msg.sender) {
-        s_rankCategory = _rankCategory;
+    constructor(
+        RankCategory _rankCategory, 
+        uint256 _costToInfluence, 
+        uint256 _costToDefluence
+    ) Ownable(msg.sender) {
+        i_rankCategory = _rankCategory;
+        i_costToInfluence = _costToInfluence;
+        i_costToDefluence = _costToDefluence;
     }
 
-    function startGame(
+    function initializeGame(
         uint256 _playerOneNFTId,
         uint256 _playersTwoNFTId,
-        address _playersBridgeAddress,
-        address[] _playerOneBetAddresses,
-        uint256[] _playerOneBetAmounts,
-        address[] _playerTwoBetAddresses,
-        uint256[] _playerTwoBetAmounts,
-
+        address _playersBridgeAddress
     ) public {
         s_playerOneNFTId = _playerOneNFTId;
         s_playerTwoNFTId = _playersTwoNFTId;
         s_playersBridgeAddress = _playersBridgeAddress;
-        s_currentRound = 1;
-        s_playerOneBetAddresses = _playerOneBetAddresses;
-        s_playerOneBetAmounts = _playerOneBetAmounts;
-        s_playerTwoBetAddresses = _playerTwoBetAddresses;
-        s_playerTwoBetAmounts = _playerTwoBetAmounts;
+        s_gameInitialized = true;
+    }
+
+    function betOnPlayerOne(uint256 _amount) external {
+        if(_amount == 0) {
+            revert Kurukshetra__InvalidBetAmount();
+        }
+        s_playerOneBetAddresses.push(msg.sender);
+        s_playerOneBetAmounts[msg.sender] += _amount;
     }
 
     function startRound(
         PlayerMoves[] memory _playerOneMoves,
         PlayerMoves[] memory _playerTwoMoves,
         address[] memory _playerOneInfluentialAddresses,
-        address[] memory _playerTwoInfluentialAddresses
-    ) {
-        if(currentRound == 0) {
+        address[] memory _playerTwoInfluentialAddresses,
+        address[] memory _playerOneDefluentialAddresses,
+        address[] memory _playerTwoDefluentialAddresses
+    ) onlyPlayersBridge {
+        if(s_currentRound == 0) {
             revert Kurukshetra__GameNotStartedYet();
         }
-        
+        if(s_currentRound >= 6) finishGame();
+        for(uint256 i = 0; i < (_playerOneDefluentialAddresses.length > _playerTwoDefluentialAddresses.length ? _playerOneDefluentialAddresses.length : _playerTwoDefluentialAddresses.length); i++) {
+            if(s_playersAlreadyUsedDefluenceAddresses[_playerOneDefluentialAddresses[i]])
+        }
     }
 
-
+    function finishGame() onlyPlayersBridge public {
+        if(s_currentRound < 6) {
+            revert Kurukshetra__GameFinishConditionNotMet();
+        }
+        // Logic to determine the winner and distribute rewards
+    }
 
     function resetGame() onlyOwner external {
         s_playerOneNFTId = 0;
         s_playerTwoNFTId = 0;
         s_playersBridgeAddress = address(0);
-        s_currentRount = 0;
+        s_currentRound = 0;
     }
 
     /* Helper Getter Functions */
 
     function getRankCategory() external view returns (RankCategory) {
-        return s_rankCategory;
+        return i_rankCategory;
     }
 }
